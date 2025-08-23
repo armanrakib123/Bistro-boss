8a8180fe1f2ce9d1f152bdd7d375ea5e063be7c5
armanrakib123 <armanrakib61@gmail.com>
2025-08-24 02:01:02 +0600

    Catagory

---
+++ src/Pages/Home/Catagory/Catagory.jsx
@@ -0,0 +1,43 @@
+import slide1 from '../../../assets/home/slide1.jpg'
+import slide2 from '../../../assets/home/slide2.jpg'
+import slide3 from '../../../assets/home/slide3.jpg'
+import slide4 from '../../../assets/home/slide4.jpg'
+import slide5 from '../../../assets/home/slide5.jpg'
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+
+import { Pagination } from 'swiper/modules';
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
